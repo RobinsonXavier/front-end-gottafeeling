e5f530194f83356ae37d68909ff98dc6003e0070
RobinsonXavier <robinson.webzen@gmail.com>
2023-01-28 20:39:34 -0300

    added new dependecies, added Welcome Page

---
+++ src/pages/welcome/index.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+function Welcome() {
+  const navigate = useNavigate();
+  return (
+    <WelcomePage>
+      <div>
+        <h1>Como está se sentindo hoje ?</h1>
+        <p>GottaFeeling é um site onde as pessoas podem se expressar e falar o que estão...</p>
+      </div>
+      <button type="submit" onClick={() => navigate('/signup')}>Cadastre-se</button>
+      <OptButton onClick={() => navigate('/signin')}>Já sou cadastrado</OptButton>
+    </WelcomePage>
+  );
+}
+
+const WelcomePage = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: var(--main-color);
+  width: var(--navpage-div-width);
+  height: var(--navpage-div-height);
+  border-radius: var(--border-radius-pattern);
+
+  div {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    height: 70%;
+    width: 80%;
+    text-align: center;
+  }
+
+  h1 {
+    margin: 15px;
+    color: var(--font-color);
+  }
+
+  p {
+    margin: 5px;
+    color: var(--font-color);
+  }
+
+  button {
+    width: 30%;
+    height: 48px;
+    border: none;
+    border-radius: var(--border-radius-pattern);
+    color: var(--font-color);
+    :hover {
+      background-color: var(--hover-color);
+    }
+  }
+`;
+const OptButton = styled.button`
+  background-color: var(--main-color);
+  text-decoration: underline;
+  :hover {
+    background-color: var(--main-color) !important;
+    color: var(--hover-color);
+  }
+`;
+
+export default Welcome;
